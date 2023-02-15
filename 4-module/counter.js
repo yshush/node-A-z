94d4f107923eb9bb17d0ea9c4625046fb41c0f5e
yshush <tjdgns7533@gmail.com>
2023-02-16 08:42:26 +0900

    module

---
+++ 4-module/counter.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+function increase() {
+  count++;
+}
+
+function getCount() {
+  return count;
+}
+
+module.exports.getCount = getCount;
+//module.exports.increase = increase;
+console.log(module.exports === exports);
+exports = {};
+exports.increase = increase;
+console.log(module);
