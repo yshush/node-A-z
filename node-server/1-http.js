92ee97aeb9130b9db32787d90faa22f56090d93b
yshush <tjdgns7533@gmail.com>
2023-02-26 19:39:44 +0900

    html

--- node-server/1-http.js
+++ node-server/1-http.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 // const http2 = require('http2');  // https
 
 //console.log(http.STATUS_CODES);
@@ -10,8 +11,15 @@ const server = http.createServer((req, res) => {
   console.log(req.httpVersion);
   console.log(req.method);
   console.log(req.url);
-  res.write('Welcome!');
-  res.end();
+  res.setHeader('Content-Type', 'text/html');
+  const url = req.url;
+  if (url === '/') {
+    fs.createReadStream('./html/index.html').pipe(res);
+  } else if (url === '/courses') {
+    fs.createReadStream('./html/courses.html').pipe(res);
+  } else {
+    fs.createReadStream('./html/not-found.html').pipe(res);
+  }
 });
 
 server.listen(8080);
