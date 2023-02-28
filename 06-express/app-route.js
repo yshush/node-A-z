7c9975ff529ead6e6839e09873b8b76a1e2822d6
yshush <tjdgns7533@gmail.com>
2023-02-28 19:50:38 +0900

    common middlewares

--- 06-express/app-route.js
+++ 06-express/app-route.js
@@ -4,7 +4,20 @@ import userRouter from './router/user.js';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // REST API -> Body
+app.use(express.urlencoded({ extended: false })); // HTML Form -> Body
+
+const options = {
+  dotfiles: 'ignore',
+  etag: false,
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now());
+  },
+};
+app.use(express.static('public', options));
 
 app.use('/posts', postRouter);
 app.use('/users', userRouter);
