2ee30eb4d973557823db3aa41e5fe7803f0f411f
yshush <tjdgns7533@gmail.com>
2023-03-02 22:12:52 +0900

    cors

---
+++ 7-cors/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ['http://127.0.0.1:5500'],
+    optionsSuccessStatus: 200,
+    credentials: true, // Access-Control-Allow-Credentials: true
+  })
+);
+
+app.get('/', (req, res) => {
+  res.send('Welcome!');
+});
+
+app.listen(8080);
