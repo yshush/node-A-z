67a15fe58273fb0a41024178ef864a47d3c2a563
yshush <tjdgns7533@gmail.com>
2023-03-03 09:00:32 +0900

    useful - externals

--- 7-cors/app.js
+++ 7-cors/app.js
@@ -1,17 +1,26 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ['http://127.0.0.1:5500'],
-    optionsSuccessStatus: 200,
-    credentials: true, // Access-Control-Allow-Credentials: true
-  })
-);
+const corsOption = {
+  origin: ['http://127.0.0.1:5500'],
+  optionsSuccessStatus: 200,
+  credentials: true, // Access-Control-Allow-Credentials: true
+};
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('tiny'));
+app.use(cors(corsOption));
+app.use(helmet());
 
 app.get('/', (req, res) => {
+  console.log(req.body);
+  console.log(req.cookies);
   res.send('Welcome!');
 });
 
