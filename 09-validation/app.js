68391380b4254e7b54b4d1af614531652d5bf4bd
yshush <tjdgns7533@gmail.com>
2023-03-06 04:49:14 +0900

    express-validator

---
+++ 09-validation/app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import { body, validationResult, param } from 'express-validator';
+
+const app = express();
+app.use(express.json());
+
+app.post(
+  '/users',
+  [
+    body('name').isLength({ min: 2 }).withMessage('이름은 두글자 이상!'),
+    body('age').isInt().withMessage('숫자를 입력해'),
+    body('email').isEmail().withMessage('이메일 입력해요'),
+    body('job.name').notEmpty(),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+    console.log(req.body);
+    res.sendStatus(201);
+  }
+);
+
+app.get(
+  '/:email',
+  param('email').isEmail().withMessage('이메일 입력해요'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+    res.send('💌');
+  }
+);
+
+app.listen(8080);
