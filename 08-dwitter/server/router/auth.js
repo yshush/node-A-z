4eabbc4816d6bcc35c5cf5391998fe1e1e689396
yshush <tjdgns7533@gmail.com>
2023-03-10 07:35:00 +0900

    server - sign up, login RestAPIs

---
+++ 08-dwitter/server/router/auth.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import {} from 'express-async-errors';
+import { body } from 'express-validator';
+import { validate } from '../middleware/validator.js';
+import * as authController from '../controller/auth.js';
+
+const router = express.Router();
+
+const validateCredential = [
+  body('username')
+    .trim()
+    .notEmpty()
+    .withMessage('username should be at least 5 characters'),
+  body('password')
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage('password should be at least 5 characters'),
+  validate,
+];
+
+const validateSignup = [
+  ...validateCredential,
+  body('name').notEmpty().withMessage('name is missing'),
+  body('email').isEmail().normalizeEmail().withMessage('invalid email'),
+  body('url').isURL().withMessage({ nullable: true, checkFalsy: true }),
+  validate,
+];
+
+router.post('/signup', validateSignup, authController.signup);
+
+router.post('/login', validateCredential, authController.login);
+
+export default router;
